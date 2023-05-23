f360d8808653cb3716c3deade5f43076e1e1fc69
Michael-Buser-SDI <54960764+Michael-Buser-SDI@users.noreply.github.com>
2023-05-23 17:11:48 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,7 @@ mkdir ~/.config/nvim/lua/user
 cp ./init.lua ~/.config/nvim/lua/user/
 cp ./.dotfiles/.tmux.conf ~/
 
-yes | sudo apt-get install neovim tmux
+yes | sudo apt-get install neovim tmux ripgrep
+
+git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
+nvim
