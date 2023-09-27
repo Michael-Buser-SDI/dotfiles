39e6d938f4a3e634ad6d59bb1d11edac5c784248
Michael Buser <54960764+Michael-Buser-SDI@users.noreply.github.com>
2023-09-27 12:59:56 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,9 @@
-cp ./.dotfiles/.tmux.conf ~/
+sudo apt update
 
-yes | sudo apt-get install tmux ripgrep
-
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod u+x nvim.appimage
-./nvim.appimage --appimage-extract
-sudo mv squashfs-root /
-sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
-rm nvim.appimage
+yes | sudo apt-get install neovim
 
 git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
-mkdir ~/.config/nvim/lua/user
+
 cp ./.dotfiles/init.lua ~/.config/nvim/lua/user/init.lua
 
-sudo wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/SpaceMono.zip -P /usr/share/fonts
-sudo mkdir /usr/share/fonts/SpaceMono
-sudo unzip /usr/share/fonts/SpaceMono.zip -d /usr/share/fonts/SpaceMono
-sudo rm -rf /usr/share/fonts/SpaceMono.zip
+nvim
