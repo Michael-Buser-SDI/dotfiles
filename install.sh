14401e62f8bdfb1e29d0b87ed0960dc46b53cb82
Michael Buser <54960764+Michael-Buser-SDI@users.noreply.github.com>
2023-09-27 13:41:11 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
-sudo apt update
+cat .tmux.conf > $HOME/.tmux.conf
 
-yes | sudo apt-get install neovim
+yes | sudo apt-get install tmux ripgrep
+
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+./nvim.appimage --appimage-extract
+sudo mv squashfs-root /
+sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+rm nvim.appimage
 
 git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
-
-# cp ./.init.lua ~/.config/nvim/lua/user/init.lua
-
-#nvim
+mkdir ~/.config/nvim/lua/user
+cat .init.lua > $HOME/.config/nvim/lua/user/init.lua
