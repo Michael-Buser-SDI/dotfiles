c79518895e8f840654014721c96759b0eba5caec
Michael Buser <54960764+Michael-Buser-SDI@users.noreply.github.com>
2023-09-27 13:15:55 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ yes | sudo apt-get install neovim
 
 git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
 
-cp ./.dotfiles/init.lua ~/.config/nvim/lua/user/init.lua
+# cp ./.init.lua ~/.config/nvim/lua/user/init.lua
 
 nvim
